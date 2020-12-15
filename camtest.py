79fa30a3a8816d22ee86307e5af7b4e06fc084fd
eliusgraff <eliusagraff@gmail.com>
2020-12-15 10:50:11 -0800

    getting attributes of cam down

--- camtest.py
+++ camtest.py
@@ -1,1 +1,16 @@
 #here is the start right here
+import io
+import time
+import picamera
+
+my_stream = io.BytesIO()
+
+with picamera.PiCamera() as camera:
+	camera.resolution = (640,480)
+	camera.framerate = 10
+	camera.start_preview()
+	camera.capture(my_stream, 'jpeg')
+	time.sleep(10)
+	camera.stop_preview()
+
+my_stream.close()
